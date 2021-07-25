b30b031d6e0ce9ada7e6fe6e4920cd80071fdf23
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2021-07-25 14:45:07 -0400

    fix issue w/ script tags having 'type' attribute

--- examples/helix-basic-aspnetcore/src/Project/BasicCompany/rendering/MyPlaceholderJsonConverter.cs
+++ examples/helix-basic-aspnetcore/src/Project/BasicCompany/rendering/MyPlaceholderJsonConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Sitecore.LayoutService.Client.Response.Model;
 
 namespace BasicCompany.Project.BasicCompany
@@ -11,8 +12,27 @@ namespace BasicCompany.Project.BasicCompany
     {
         public override Placeholder ReadJson(JsonReader reader, Type objectType, Placeholder existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            Console.WriteLine("Got me!");
-            return base.ReadJson(reader, objectType, existingValue, hasExistingValue, serializer);
+            var collection = existingValue ?? new Placeholder();
+            var jArray = JArray.Load(reader);
+
+            foreach (var entry in jArray)
+            {
+                var componentName = entry["componentName"]?.ToString();
+                var elementName = entry["name"]?.ToString();
+
+                // closer to what JavaScript SDKs do
+                // https://github.com/Sitecore/jss/blob/9114d2a5ae6cbba90a4dc0ce2486e1d20a68fa4d/packages/sitecore-jss-react/src/components/PlaceholderCommon.tsx#L144-L149
+                if (string.IsNullOrEmpty(componentName) && !string.IsNullOrEmpty(elementName))
+                {
+                    collection.Add(serializer.Deserialize<EditableChrome>(entry.CreateReader()));
+                }
+                else if (!string.IsNullOrEmpty(componentName))
+                {
+                    collection.Add(serializer.Deserialize<Component>(entry.CreateReader()));
+                }
+            }
+
+            return collection;
         }
     }
 }
