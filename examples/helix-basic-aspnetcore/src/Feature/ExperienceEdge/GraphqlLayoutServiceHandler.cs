a193646917fbb7ed22c23ffad3b95157bc310286
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2021-09-13 17:01:12 -0400

    initial graphql client attempt w/ strawberry shake
    * seems to require .NET 5 so will be reverting most of this
    * commit includes a quick and dirty reverse proxy which allowed using their CLI w/ Edge

---
+++ examples/helix-basic-aspnetcore/src/Feature/ExperienceEdge/GraphqlLayoutServiceHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sitecore.LayoutService.Client.Request;
+using Sitecore.LayoutService.Client.Response;
+
+namespace BasicCompany.Feature.ExperienceEdge
+{
+    public class GraphqlLayoutServiceHandler : Sitecore.LayoutService.Client.ILayoutRequestHandler
+    {
+        public Task<SitecoreLayoutResponse> Request(SitecoreLayoutRequest request, string handlerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
